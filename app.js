9d8825490dc5ba9e265dfcd208285c390cd0719f
ajlopez <webmaster@ajlopez.com>
2015-03-05 14:40:02 -0300

    Fix response in send internal error v2

--- app.js
+++ app.js
@@ -318,7 +318,7 @@ function handleRequestV2(req, response) {
     
     scanner.scan(urlToAnalyze, user, password, deep, function (err, data) {
         if (err) {
-            sendInternalServerError(err);
+            sendInternalServerError(err, response);
             return;
         }
         
