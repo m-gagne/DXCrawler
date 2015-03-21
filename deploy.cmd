c218d6f346c4694af4ddcf9688b984f4d893f746
JuanAr <juan.arguello@southworks.net>
2015-03-21 17:46:21 -0300

    fixed deploy script

--- deploy.cmd
+++ deploy.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 
 IF EXIST sites-scanner.PublishSettings (
-	tools\WAWSDeploy\WAWSDeploy.exe dev sites-scanner.PublishSettings
+	tools\WAWSDeploy\WAWSDeploy.exe .\ sites-scanner.PublishSettings
 ) ELSE ( ECHO The file 'sites-scanner.PublishSettings' including credentials does not exists!
 PAUSE )
